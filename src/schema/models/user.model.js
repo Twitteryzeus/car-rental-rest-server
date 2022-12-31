e59edf7d11ef340627f82685e7eff26aba108b6b
Twitteryzeus <bvlakkad@gmail.com>
2022-12-31 11:42:17 +0530

    add models for user module

--- src/schema/models/user.model.js
+++ src/schema/models/user.model.js
@@ -7,12 +7,40 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     name: {
       type: DataTypes.STRING
-    }
+    },
+    phoneNo: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('ADMIN','USER'),
+      defaultValue: 'USER'
+    },
+    opt: {
+      type: DataTypes.INTEGER
+    },
+    optExpiry: {
+      type: DataTypes.DATE
+    },
+    geoLocation: {
+      type: DataTypes.GEOGRAPHY('POINT')
+    },
   }, {
     sequelize,
     modelName: 'User',
     tableName: 'user'
   })
+
+  User.associate = (models) => {
+    User.hasMany(models.UserSession, {
+      as: 'userSessions',
+      foreignKey: 'userId',
+      sourceKey: 'id',
+      constraints: false,
+      onDelete: 'RESTRICT',
+      hooks: true
+    })
+  }
 
   return User;
 };
